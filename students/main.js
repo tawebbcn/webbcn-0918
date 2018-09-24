036354b6a282be78880083c773a4dac260efb638
Alexandra Pedroche <alexandrapedroche@gmail.com>
2018-09-24 12:53:21 +0200

    add function main

--- students/main.js
+++ students/main.js
@@ -1,7 +1,12 @@
+function main () { 
 var section = document.querySelector("section.experiments");
 section.classList.add("hidden");
 var button = document.querySelector("button");
 button.addEventListener("click",function() {
   section.classList.toggle("hidden");
 })
+}
+window.addEventListener("load", "main");
+
+
 
