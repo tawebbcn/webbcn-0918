273ff27a6466408a999af26a5d693c33c182f9ff
iban Dasca <ibandasca@gmail.com>
2018-09-24 12:51:02 +0200

    Add main function and change button text event

--- students/iban.js
+++ students/iban.js
@@ -1,15 +1,31 @@
-//seleccionar elementos
-var one = document.querySelector('.primary-button');
-var dos = document.querySelector('.second-button');
-var sectionIntroduce = document.querySelector('.introduce');
-var sectionExperimentos = document.querySelector('.experiments');
+'use strict';
+
+function main(){
+    //seleccionar elementos
+    var one = document.querySelector('.primary-button');
+    var dos = document.querySelector('.second-button');
+    var sectionIntroduce = document.querySelector('.introduce');
+    var sectionExperimentos = document.querySelector('.experiments');
 
 
-one.addEventListener('click', function(e){
-    sectionIntroduce.classList.toggle('hidden');
-    
-});
+    one.addEventListener('click', function(e){
+        sectionIntroduce.classList.toggle('hidden');
+        if(sectionIntroduce.classList.contains('hidden')){
+            e.currentTarget.innerText = 'Mostrar sección presentación';
+        } else{
+            e.currentTarget.innerText = 'Esconder sección presentación';
+        }
+    });
 
-dos.addEventListener('click', function(e){
-    sectionExperimentos.classList.toggle('hidden');
-})
+    dos.addEventListener('click', function(e){
+        sectionExperimentos.classList.toggle('hidden');
+        if(sectionExperimentos.classList.contains('hidden')){
+            e.currentTarget.innerText = 'Mostrar Experimentos';
+        } else{
+            e.currentTarget.innerText = 'Esconder Experimentos';
+        }
+    });
+}
+
+
+window.addEventListener('load', main);
