4a0144da8b560ae794aa16f8fc551030643c65ee
Eva Panizo <panizo.eva@gmail.com>
2018-09-24 12:56:58 +0200

    Add main and button text changes

--- students/evaa.js
+++ students/evaa.js
@@ -1,11 +1,20 @@
-var hideButtons = document.querySelectorAll(".hide-button");
-var sections = document.querySelectorAll(".to-hide-info");
+'use strict';
 
-var hideSection = function (event) {
-  event.currentTarget.parentNode.parentNode.children[1].classList.toggle("hidden");
+function main (){
+  var hideButtons = document.querySelectorAll(".hide-button");
+
+  var hideSection = function (event) {
+    event.currentTarget.parentNode.parentNode.children[1].classList.toggle("hidden");
+    if (event.currentTarget.parentNode.parentNode.children[1].classList.contains("hidden")){
+      event.currentTarget.innerText = "Show section";
+    } else {
+      event.currentTarget.innerText = "Hide section";
+    }
+  }
+
+  hideButtons.forEach(function(hideButton){
+    hideButton.addEventListener('click', hideSection);
+  });
 }
 
-hideButtons.forEach(function(hideButton){
-  hideButton.addEventListener('click', hideSection);
-});
-  
+window.addEventListener("load", main);
