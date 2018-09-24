f486157f1d35dc08bd64d43227e52440db7fd397
Ester Diaz <ester.diaz.sola@gmail.com>
2018-09-24 12:58:16 +0200

    add buttons DOM

---
+++ students/ester.js
@@ -0,0 +1,36 @@
+function main() {
+
+var section = document.querySelector('.informacion-personal');
+var button = document.querySelector('.toggle-info');
+var section2 = document.querySelector('.experiments');
+var button2 = document.querySelector('.toggle-experiments');
+
+function handleToggleSection(e) {
+  section.classList.toggle('oculto');
+  if (section.classList.contains('oculto')){
+    e.currentTarget.innerText = 'Show header';
+  } else {
+    e.currentTarget.innerText = 'Hide header';
+  }
+}
+
+function handleToggleSection2(e) {
+  section2.classList.toggle('oculto');
+  if (section2.classList.contains('oculto')){
+    e.currentTarget.innerText = 'Show header';
+  } else {
+    e.currentTarget.innerText = 'Hide header';
+  }
+}
+
+button.addEventListener('click', handleToggleSection);
+button2.addEventListener('click', handleToggleSection2);
+
+}
+
+window.addEventListener('load', main);
+
+
+
+
+
