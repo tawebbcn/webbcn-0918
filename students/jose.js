ce99507e04cf9fe7bdcfe3be28798b7376a7b7fd
elegidoadedo <jmoralesmnz@gmail.com>
2018-09-24 12:52:25 +0200

    added toggle text

--- students/jose.js
+++ students/jose.js
@@ -3,8 +3,19 @@ var li2= document.querySelector(".btnder");
 var sectionizq= document.querySelector(".perfil") 
 li1.addEventListener('click', function(){
   sectionizq.classList.toggle("hidden");
+  if(sectionizq.classList.contains('hidden')){
+    li1.innerText = "Muestra la sección";
+  } else{
+      li1.innerText = "Esconde la sección"; 
+    }
 })
-var sectionder= document.querySelector(".experimentos") 
+var sectionder= document.querySelector(".experimentos");
+
 li2.addEventListener('click', function(){
   sectionder.classList.toggle("hidden");
-})
+  if(sectionder.classList.contains('hidden')){
+    li2.innerText = "Muestra la sección";
+  } else{
+      li2.innerText = "Esconde la sección"; 
+    }
+  })
