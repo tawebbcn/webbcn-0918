28f2d864eeccbfd2daa1570fb7cc5a345bf16576
Bjornhona <asaeri3@hotmail.com>
2018-09-26 00:00:33 +0200

    added css and js files

--- students/asa.js
+++ students/asa.js
@@ -2,41 +2,122 @@
 function main() {
 
   var profile = document.querySelector('section.profile');
+  var about = document.querySelector('section.about');
   var experiments = document.querySelector('section.experiments');
-  var header = document.querySelector('header.header');
   var firstButton = document.createElement('button');
   var secondButton = document.createElement('button');
 
   firstButton.classList.add('first-button');
   secondButton.classList.add('second-button');
 
-  firstButton.innerText = 'Hide section';
-  secondButton.innerText = 'Hide section';
+  firstButton.innerText = 'Hide first section';
+  secondButton.innerText = 'Hide second section';
 
-  header.appendChild(firstButton);
-  header.appendChild(secondButton);
+  about.appendChild(firstButton);
+  about.appendChild(secondButton);
 
   function hideFirstSection(element) {
-    profile.classList.toggle('hidden');
-    if (profile.classList.contains('hidden')) {
-      element.currentTarget.innerText = 'Show header';
+    about.classList.toggle('hidden');
+    if (about.classList.contains('hidden')) {
+      element.currentTarget.innerText = 'Show first section';
     } else {
-      element.currentTarget.innerText = 'Hide header';
+      element.currentTarget.innerText = 'Hide first section';
     }
   }
 
   function hideSecondSection(element) {
     experiments.classList.toggle('hidden');
     if (experiments.classList.contains('hidden')) {
-      element.currentTarget.innerText = 'Show header';
+      element.currentTarget.innerText = 'Show second section';
     } else {
-      element.currentTarget.innerText = 'Hide header';
+      element.currentTarget.innerText = 'Hide second section';
     }
   }
 
   firstButton.addEventListener('click', hideFirstSection);
   secondButton.addEventListener('click', hideSecondSection);
 
+
+/* ---- searchBar ---- */
+
+var input = document.querySelector('.input-student input');
+input.addEventListener('keyup', handleKeyDown);
+/* Jag lägger till denna nedan för att det ska gå att klicka i input-boxen utan att eventListener tar bort sökningen. */
+input.addEventListener('click', function () {
+  event.stopPropagation();
+})
+var searchResultsElement = document.querySelector('.search-results');
+
+document.body.addEventListener('keyup', function (event) {
+  if (event.key === 'Escape') {
+    searchResultsElement.innerHTML = '';
+  }
+});
+
+document.body.addEventListener('click', function () {
+  searchResultsElement.innerHTML = '';
+  input.value = '';
+});
+
+function findStudents (searchTerm) {
+  var results = [];
+  if (searchTerm) {
+    results = students.filter(function (student) {
+      var studentName = student.name.toLowerCase();
+      if (studentName.indexOf(searchTerm) !== -1) {
+        return true;
+      }
+    });
+  }
+  return results;
 }
 
+function displayResults (results) {
+  searchResultsElement.innerHTML = '';
+  var searchResultsListElement = document.createElement('ul');
+  results.forEach(function(result) {
+    var resultLink = document.createElement('a');
+    resultLink.innerText = result.name;
+    resultLink.setAttribute('href', '../' + result.url);
+    var resultListItem = document.createElement('li');
+    resultListItem.appendChild(resultLink);
+    searchResultsListElement.appendChild(resultListItem);
+  })
+  searchResultsElement.appendChild(searchResultsListElement);
+}
+
+function handleKeyDown () {
+  var searchTerm = input.value.toLowerCase();
+  var results = findStudents(searchTerm);
+  displayResults(results);
+}
+
+/* ---- async ---- */
+
+var getLost = document.querySelector('.dont-push-button');
+var getBack = document.querySelector('.push-button');
+var main = document.querySelector('main');
+var timerId;
+
+getLost.addEventListener('click', function () {
+  main.classList.add('hidden');
+  getBack.classList.remove('hidden');
+  timerId = setTimeout(function () {
+    main.classList.remove('hidden');
+    getBack.classList.add('hidden');
+  }, 3000);
+});
+
+getBack.addEventListener('click', function () {
+  main.classList.remove('hidden');
+  getBack.classList.add('hidden');
+  clearTimeout(timerId);
+});
+
+
+
+}
 window.addEventListener('load', main);
+
+
+// "https://healthy-websites.com/design-53926.html"
