ed42dc89cc17d454aac71de875bcf67d43782357
Eva Panizo <panizo.eva@gmail.com>
2018-09-26 00:40:00 +0200

    Search engine, games section and CSS styles added

--- students/evaa.js
+++ students/evaa.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function main (){
+
+  //Hide Sections
   var hideButtons = document.querySelectorAll(".hide-button");
 
   var hideSection = function (event) {
@@ -15,6 +17,129 @@ function main (){
   hideButtons.forEach(function(hideButton){
     hideButton.addEventListener('click', hideSection);
   });
+
+  //Move Picture
+  var profilePicture = document.querySelector(".profile-picture");
+
+  var motionPicture = function (event) {
+    profilePicture.classList.toggle("animation-picture");
+  }
+
+  profilePicture.addEventListener('mouseover', motionPicture);
+  profilePicture.addEventListener('mouseleave', motionPicture);
+
+  // Search Engine
+  var input = document.querySelector(".input-student input");
+  var searchResultsElement = document.querySelector(".search-results");
+  searchResultsElement.innerHTML = [];
+
+  input.addEventListener('click', function (event) {
+    event.stopPropagation();
+  });
+
+  document.body.addEventListener('click', function () {
+    searchResultsElement.innerHTML = "";
+  });
+
+  document.body.addEventListener('keyup', function (event) {
+    if (event.key === 'Escape'){
+      searchResultsElement.innerHTML = "";
+      return;
+    }
+  });
+
+  var findStudents = function (searchTerm) {
+    var results = [];
+    if (searchTerm){
+      results = students.filter(function (student){
+        var studentName = student.name.toLowerCase();
+        if (studentName.indexOf(searchTerm) !== -1){
+          return true;
+        }
+      });
+    }
+    return results;
+  }
+
+  var displayResults = function (results) {
+    var searchResultsListElement = document.createElement('ul');
+    searchResultsElement.innerHTML = [];
+    results.forEach(function (result){
+      var resultLink = document.createElement("a");
+      resultLink.innerText = result.name;
+      resultLink.setAttribute("href", "../" + result.url);
+      var resultListItem = document.createElement("li");
+      resultListItem.appendChild(resultLink);
+      searchResultsListElement.appendChild(resultListItem);
+    })
+    searchResultsElement.appendChild(searchResultsListElement);
+  } 
+  
+  var handleKeyUp = function (event) {
+    var searchTerm = input.value.toLowerCase();
+    var results = findStudents(searchTerm);
+    displayResults(results);
+  }
+  
+  input.addEventListener('keyup', handleKeyUp);
+
+  //Game - Password
+  var secondsCount = 20;
+  var timeLeft;
+  var myPassword = 'password';
+  var playButton = document.querySelector(".password-play-button");
+  var gameContainer = document.querySelector(".game-container");
+  var countdown = document.createElement('p');
+  countdown.innerText = secondsCount;
+  var userGuess = document.createElement('input');
+  userGuess.setAttribute('placeholder', "Escribe una contraseña")
+
+  var activatePasswordGame = function (event) {
+    event.stopPropagation();
+    timeLeft = secondsCount;
+    function checkAnswer (event) {
+      event.stopPropagation();
+      if (event.key === "Enter"){
+        if (userGuess.value === myPassword){
+          countdown.classList.remove("timeup");
+          countdown.classList.add("win");
+          countdown.innerText = "CORRECT!!!";
+          clearInterval(intervalId);
+          setTimeout(endGame, 2000);
+        }
+      }
+    }
+    userGuess.addEventListener('keyup', checkAnswer);
+    playButton.removeEventListener("click",activatePasswordGame)
+    gameContainer.appendChild(countdown);
+    gameContainer.appendChild(userGuess);
+    var intervalId = setInterval(function (){
+      countdown.innerText = timeLeft;
+      if(timeLeft === 5){
+        countdown.classList.add("timeup");
+      }
+      if(!timeLeft){
+        countdown.innerText = "GAME OVER";
+        clearInterval(intervalId);
+        setTimeout(endGame, 2000);
+      }
+      timeLeft--;
+    }, 1000);
+  }
+  
+  function endGame(){
+    countdown.classList.remove("timeup");
+    countdown.classList.remove("win");
+    playButton.addEventListener('click', activatePasswordGame);
+    userGuess.value = '';
+    countdown.innerText = secondsCount;
+    countdown.remove();
+    userGuess.remove();
+  }
+
+  playButton.addEventListener('click', activatePasswordGame);
+
+
 }
 
 window.addEventListener("load", main);
